b5da539d016734f833cad222b342361a94a36877
Aleksey Klimov <alx111wrk@gmail.com>
2023-11-21 16:38:01 +0300

    init

---
+++ src/components/Converter.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { hex2rgb, validHex } from "../utils/validators";
+
+
+export const Converter = () => {
+  const [valueRgb, setValueRgb] = useState('rgb');
+  const [bcgColor, setBcgColor] = useState('grey');
+
+  function handlerColor({target}: React.ChangeEvent<HTMLInputElement>) {
+    const inputValue = target.value;
+
+    if (inputValue.length >= 7 && !validHex(inputValue)) {
+      setValueRgb('Ошибка!');
+      setBcgColor('RGB(233, 75, 53)');
+    } else if (inputValue.length === 7 && validHex(inputValue)) {
+      setBcgColor(inputValue);
+      setValueRgb(hex2rgb(inputValue));
+    } else {
+      setValueRgb('rgb');
+      setBcgColor('grey');
+    }
+  }
+
+  return (
+    <div className="box" style={{backgroundColor: `${bcgColor}`}}>
+      <input 
+        className="hex-input"
+        type="text"
+        onChange={handlerColor}
+      />
+      <div className="rgb-result">{valueRgb}</div>
+    </div>
+  )
+}
+
+
+// #9A00FF
